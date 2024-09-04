f8d670ef28c198aec033f58e92852c5c1bd5cec6
suprohub <suprohub@gmail.com>
2024-09-04 18:41:20 +0400

    no async/await

--- cell_update.js
+++ cell_update.js
@@ -1,12 +1,11 @@
 
-(async function() {
-    await fetch(`${window.location.href}join`, {
-        method: "POST"
-    }).then(async response => {
-        const player_id = await response.text();
+fetch(`${window.location.href}join`, {
+    method: "POST"
+}).then(response => {
+    response.text().then(player_id => {
         var move = (player_id == 0)
         console.log(player_id, move)
-
+    
         document.addEventListener("click", (mouse_event) => {
             console.log(move)
             if (move) {
@@ -24,29 +23,33 @@
         var last_data = "";
         
         for (loop=true ; loop ; ) {
-            fetch(`${window.location.href}cell_sync`, {
-                method: "POST"
-            }).then(async response => {
-                var data = (await response.text()).split(" ")
-                if (data[1] == -2) {
-                    const text = document.getElementById("win");
-                    text.innerHTML = `Выйграл игрок номер ${data[0]}`
-                    loop = false
-                } else if (data[0] != -1 && last_data != data[0]) {
-                    const button = document.getElementById(`b${data[0]}`)
-                    button.setAttribute("disabled", true)
-                    button.innerHTML = data[1]
-                    last_data = data[0]
-                    console.log(data[1], player_id, (player_id === 0)? 1 : player_id - 1)
-                    if (data[1] == (player_id === 0)? 1 : player_id - 1) {
-                        move = true
-                    }
-                }
+            sleep(100).then(() => {
+                fetch(`${window.location.href}cell_sync`, {
+                    method: "POST"
+                }).then(response => {
+                    response.text.then((data) => {
+                        data = data.split(" ")
+                        
+                        if (data[1] == -2) {
+                            const text = document.getElementById("win");
+                            text.innerHTML = `Выйграл игрок номер ${data[0]}`
+                            loop = false
+                        } else if (data[0] != -1 && last_data != data[0]) {
+                            const button = document.getElementById(`b${data[0]}`)
+                            button.setAttribute("disabled", true)
+                            button.innerHTML = data[1]
+                            last_data = data[0]
+                            console.log(data[1], player_id, (player_id === 0)? 1 : player_id - 1)
+                            if (data[1] == (player_id === 0)? 1 : player_id - 1) {
+                                move = true
+                            }
+                        }
+                    })
+                })
             })
-            await sleep(100)
-        }
+        }  
     })
-})()
+})
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
