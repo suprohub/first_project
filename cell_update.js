15a8959b5a5a092fd53dccd8fcc9212670185e52
suprohub <suprohub@gmail.com>
2024-09-04 19:59:13 +0400

    fixes

--- cell_update.js
+++ cell_update.js
@@ -1,55 +1,50 @@
+var player_id = 'constant_player_id';
 
-fetch(`${window.location.href}join`, {
-    method: "POST"
-}).then(response => {
-    response.text().then(player_id => {
-        var move = (player_id == 0)
-        console.log(player_id, move)
-    
-        document.addEventListener("click", (mouse_event) => {
-            console.log(move)
-            if (move) {
-                const button_id = mouse_event.target.id;
-                if (button_id) {
-                    const button = document.getElementById(button_id)
-                    fetch(`${window.location.href}cell_update`, {
-                        method: "POST",
-                        body: `${button_id} ${player_id}`
-                    })
-                    move = false
-                }
-            }
-        })
-        var last_data = "";
-        
-        for (loop=true ; loop ; ) {
-            sleep(100).then(() => {
-                fetch(`${window.location.href}cell_sync`, {
-                    method: "POST"
-                }).then(response => {
-                    response.text.then((data) => {
-                        data = data.split(" ")
-                        
-                        if (data[1] == -2) {
-                            const text = document.getElementById("win");
-                            text.innerHTML = `Выйграл игрок номер ${data[0]}`
-                            loop = false
-                        } else if (data[0] != -1 && last_data != data[0]) {
-                            const button = document.getElementById(`b${data[0]}`)
-                            button.setAttribute("disabled", true)
-                            button.innerHTML = data[1]
-                            last_data = data[0]
-                            console.log(data[1], player_id, (player_id === 0)? 1 : player_id - 1)
-                            if (data[1] == (player_id === 0)? 1 : player_id - 1) {
-                                move = true
-                            }
-                        }
-                    })
-                })
+var move = (player_id == 0)
+console.log(player_id, move)
+
+document.addEventListener("click", (mouse_event) => {
+    console.log(move)
+    if (move) {
+        const button_id = mouse_event.target.id;
+        if (button_id) {
+            const button = document.getElementById(button_id)
+            fetch(`${window.location.href}cell_update`, {
+                method: "POST",
+                body: `${button_id} ${player_id}`
             })
-        }  
-    })
+            move = false
+        }
+    }
 })
+var last_data = "";
+
+for (loop=true ; loop ; ) {
+    sleep(100).then(() => {
+        fetch(`${window.location.href}cell_sync`, {
+            method: "POST"
+        }).then(response => {
+            response.text.then((data) => {
+                data = data.split(" ")
+                
+                if (data[1] == -2) {
+                    const text = document.getElementById("win");
+                    text.innerHTML = `Выйграл игрок номер ${data[0]}`
+                    loop = false
+                } else if (data[0] != -1 && last_data != data[0]) {
+                    const button = document.getElementById(`b${data[0]}`)
+                    button.setAttribute("disabled", true)
+                    button.innerHTML = data[1]
+                    last_data = data[0]
+                    console.log(data[1], player_id, (player_id === 0)? 1 : player_id - 1)
+                    if (data[1] == (player_id === 0)? 1 : player_id - 1) {
+                        move = true
+                    }
+                }
+            })
+        })
+    })
+}  
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
--- index.ts
+++ index.ts
@@ -1,106 +1,136 @@
+import * as http from "node:http";
+import * as fs from "node:fs";
 
-import * as http from 'node:http'
-import * as fs from 'node:fs'
-import { exit } from 'node:process';
-
-const buttons_table = `<!DOCTYPE html><meta charset="UTF-8"><script>${fs.readFileSync("cell_update.js")}</script>` + generate_table() + `<p1 id="win"></p1>`;
-const field_len = 20;
+const game =
+	`<!DOCTYPE html><meta charset="UTF-8"><script>${fs.readFileSync("cell_update.js")}</script>` +
+	generate_table() +
+	`<p1 id="win"></p1>`;
+var field_len = 20;
 var table: Map<number, number> = new Map();
 var last_player = 0;
 var current_move = 0;
 var sync_data = -1;
 
-// Creating server 
+// Creating server
 const server = http.createServer((req, res) => {
-    if (req.url == "/") {
-        res.writeHead(200, { "Content-Type" : "text/html" } );
-        res.write(buttons_table)
-        res.end();
-    } else if (req.url == "/cell_update" && req.method == "POST") {
-        var data = '';
-        req.on('data', chunk => {
-          data += chunk.toString();
-        });
-        req.on('end', () => {
-            console.log('POST data:', data);
-            res.end('Data received');
-            
-            var ids: number[] = data.replace("b", "").split(" ").map(val => Number(val));
-            console.log(ids)
-            if (ids[0] < 400 && !table.has(ids[0]) && current_move == ids[1]) {
-                table.set(ids[0], ids[1])
-                sync_data = ids[0]
-                current_move++
-                if (current_move == 2) {
-                    current_move = 0
-                }
-                var check = win_check(ids[0]);
-                if (!Number.isNaN(check)) {
-                    sync_data = check
-                    current_move = -1
-                }
-            } else {
-                console.log(`cheater is ${req.socket.remoteAddress}`)
-            }
-        });
-    } else if (req.url == "/cell_sync" && req.method == "POST") {
-        var old_move = current_move - 1;
-        if (old_move == -1) {
-            old_move = 1
-        }
-        res.end(`${sync_data} ${old_move}`)
-    } else if (req.url == "/join" && req.method == "POST") {
-        console.log(`new player ${last_player}`)
-        res.end(last_player.toString())
-        last_player++
-    }
-
-})
+	if (req.method == "POST") {
+		switch (req.url) {
+			case "/cell_update": {
+                receive_data(req, (data) => {
+                    console.log("POST data:", data);
+                    res.end("Data received");
+    
+                    var ids: number[] = data
+                        .replace("b", "")
+                        .split(" ")
+                        .map((val) => Number(val));
+                    if (ids[0] < 400 && !table.has(ids[0]) && current_move == ids[1]) {
+                        table.set(ids[0], ids[1]);
+                        sync_data = ids[0];
+                        current_move++;
+                        if (current_move == 2) {
+                            current_move = 0;
+                        }
+                        var check = is_win(ids[0]);
+                        if (!Number.isNaN(check)) {
+                            sync_data = check;
+                            current_move = -1;
+                        }
+                    } else {
+                        console.log(`intresting connection: ${req.socket.remoteAddress}`);
+                    }
+                })
+				break;
+			}
+			case "/cell_sync": {
+                console.log("sync")
+				var old_move = current_move - 1;
+				if (old_move == -1) {
+					old_move = 1;
+				}
+				res.end(`${sync_data} ${old_move}`);
+			}
+		}
+	} else {
+		switch (req.url) {
+			case "/": {
+                var local_game = game.replace("'constant_player_id'", last_player.toString());
+                console.log(local_game)
+                console.log(`new player ${last_player}`);
+				last_player++;
+				res.writeHead(200, { "Content-Type": "text/html" });
+				res.end(local_game);
+				break;
+			}
+		}
+	}
+});
 
 server.listen(3000, "localhost", () => {
-    console.log("hello");
-})
+	console.log("hello");
+});
 
-
-function win_check(cell_id: number): number {
-    var value: number = check_direction(cell_id, 0, 1) || check_direction(cell_id, 0, -1) || check_direction(cell_id, 20, 0) || check_direction(cell_id, -20, 0)
-        || check_direction(cell_id, 20, 1) || check_direction(cell_id, 20, -1) || check_direction(cell_id, -20, 1) || check_direction(cell_id, -20, -1)
-    if (!Number.isNaN(value)) {
-        return value
-    }
-    return NaN;
+function is_win(cell_id: number): number {
+	var value: number =
+		check_direction(cell_id, 0, 1)  ||
+		check_direction(cell_id, 0, -1) ||
+		check_direction(cell_id, 1, 0)  ||
+		check_direction(cell_id, -1, 0) ||
+		check_direction(cell_id, 1, 1)  ||
+		check_direction(cell_id, 1, -1) ||
+		check_direction(cell_id, -1, 1) ||
+		check_direction(cell_id, -1, -1);
+	if (!Number.isNaN(value)) {
+		return value;
+	}
+	return NaN;
 }
 
-function check_direction(first_cell: number, y_add: number, x_add: number): number {
-    var values: any[] = []
-    var value: number | undefined;
-    var x = 0
-    var y = 0
-    for (var i = 0; i < 4; i++) {
-        value = table.get(first_cell + y + x);
-        x += y_add
-        y += x_add
-        values.push(value)
-    }
-    for (var i = 0; i < last_player; i++) {
-        if (values.every((value) => value == i)) {
-            console.log("win")
-            return i
-        }
-    }
-    return NaN
+function check_direction(
+	first_cell: number,
+	y_add: number,
+	x_add: number,
+): number {
+	var values: any[] = [];
+	var value: number | undefined;
+	var [x, y] = [0, 0];
+	for (var i = 0; i < 4; i++) {
+		value = table.get(first_cell + y + x);
+		x += y_add * field_len;
+		y += x_add;
+		values.push(value);
+	}
+	for (var i = 0; i < last_player; i++) {
+		if (values.every((value) => value == i)) {
+			console.log("win");
+			return i;
+		}
+	}
+	return NaN;
 }
 
 function generate_table(): string {
-    var table = ""
-    var cell_id = 0
-    for (var row_id = 0; row_id < 20; row_id++) {
-        var row = ""
-        for (var col_id = 0; col_id < 20; col_id++) {
-            row += `<td> <button type="button" id="b${cell_id}"> &nbsp </button> </td>\n`
-            cell_id++
-        }
-        table += `<tr>${row}</tr>`
-    }
-    return `<table><tbody>${table}</tbody>\n</table>`
+	var table = ""
+    var row = ""
+	var cell_id = 0;
+	for (var row_id = 0; row_id < field_len; row_id++) {
+		for (var col_id = 0; col_id < field_len; col_id++) {
+			row += `<td> <button type="button" id="b${cell_id}"> &nbsp </button> </td>\n`;
+			cell_id++;
+		}
+		table += `<tr>${row}</tr>`;
+        row = "";
+	}
+    console.log(table, `<tr>${row}</tr>`)
+	return `<table><tbody>${table}</tbody>\n</table>`;
+}
+
+function receive_data(req, callback: (data: string) => void) {
+    var data = "";
+    req.on("data", (chunk) => {
+        data += chunk.toString();
+    });
+    req.on("end", () => {
+        callback(data)
+    });
 }
